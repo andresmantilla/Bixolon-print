be07e8fd74d47a03559aa2765390ff5446fd13e6
andresmantilla <andres.mantilla@fitideas.co>
2014-09-29 19:35:18 -0500

    dd

--- www/bixolonprint.js
+++ www/bixolonprint.js
@@ -1,15 +1,44 @@
-var bixolonPrint = {
-	print: function ( text, successCallback, errorCallback) {
-		 cordova.exec(
-            successCallback, // success callback function
-            errorCallback, // error callback function
-            'BixolonPrint', // mapped to our native Java class called "Calendar"
-            'print', // with this action name
-            [{                  // and this array of custom arguments to create our entry
-                "text": text,
-            }]
-        ); 
-    }
-}
+    var PrinterLoader = function (require, exports, module) {
 
-module.exports = bixolonPrint;
+        var exec = require("cordova/exec");
+
+        /**
+         * Constructor.
+         *
+         * @returns {BixolonPrinter}
+         */
+        function BixolonPrinter() {
+        };
+
+        BixolonPrinter.prototype.print = function (successCallback, errorCallback) {
+            if (errorCallback == null) {
+                errorCallback = function () {
+                };
+            }
+
+            if (typeof errorCallback != "function") {
+                console.log("BixolonPrinter.print failure: failure parameter not a function");
+                return;
+            }
+
+            if (typeof successCallback != "function") {
+                console.log("BixolonPrinter.print failure: success callback parameter must be a function");
+                return;
+            }
+
+            exec(successCallback, errorCallback, 'BixolonPrinter', 'print', []);
+        };
+
+
+        var bixolonPrinter = new BixolonPrinter();
+        module.exports = bixolonPrinter;
+
+    }
+
+    PrinterLoader(require, exports, module);
+
+    cordova.define("cordova/plugin/BixolonPrinter", PrinterLoader);
+
+
+
+
